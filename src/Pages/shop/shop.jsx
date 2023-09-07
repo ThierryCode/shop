81e86a97df074a5f0031d657ec2a1de1474d5edf
Andry Kamamy <tchithierry@gmail.com>
2023-09-07 14:07:21 +0300

    Mise en place des dossier et fichier jsx

---
+++ src/Pages/shop/shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Shophop() {
+  return (
+    <div className='shop'>
+        <div className="">
+            <h1>PedroTech</h1>
+        </div>
+    </div>
+  )
+}
